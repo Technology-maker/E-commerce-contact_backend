13a87af46ffbc87a230588fc76ee90adbf062fd2
Technology-maker <ys5401519@gmail.com>
2026-02-11 00:35:11 +0530

    first commit

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: [
-    "https://e-commerce-app-mu-flax.vercel.app",
-    "http://localhost:5173",
-  ],
-  methods: ["GET", "POST", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
+    origin: [
+        "https://e-commerce-app-mu-flax.vercel.app",
+        "https://e-commerce-contact-backend.vercel.app",
+        "http://localhost:5173",
+    ],
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
 }));
 
 app.options("*", cors());
@@ -21,7 +22,7 @@ app.options("*", cors());
 app.use("/api", router);
 
 app.get("/", (req, res) => {
-  res.send("Backend running");
+    res.send("Backend running");
 });
 
 export default app;
